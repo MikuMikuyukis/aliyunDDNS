706e72ee47362fa7f648cd62873d3ed437f2a7e2
zhangwei <zhangwei24@apache.org>
2021-11-25 22:20:37 +0800

    add logging

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from aliyunsdkcore.client import AcsClient
-from aliyunsdkcore.acs_exception.exceptions import ClientException
-from aliyunsdkcore.acs_exception.exceptions import ServerException
+import json
+import logging
+import time
+
+import requests
+from aliyunsdkalidns.request.v20150109.AddDomainRecordRequest import AddDomainRecordRequest
 from aliyunsdkalidns.request.v20150109.DescribeDomainRecordsRequest import DescribeDomainRecordsRequest
 from aliyunsdkalidns.request.v20150109.UpdateDomainRecordRequest import UpdateDomainRecordRequest
-from aliyunsdkalidns.request.v20150109.AddDomainRecordRequest import AddDomainRecordRequest
-import json
-import requests
-import time
-import os
-import platform
+from aliyunsdkcore.client import AcsClient
 
 settings = {}
+
+logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
+                    level=logging.INFO)
 
 
 def read_json_settings():
@@ -33,23 +34,23 @@ def add_domain_name_value(ip):
     request.set_Value(ip)
 
     response = client.do_action_with_exception(request)
-    print(str(response, encoding='utf-8'))
-    print("************新增完成************")
+    logging.info(str(response, encoding='utf-8'))
+    logging.info("新增完成")
 
 
-def update_domain_name_value(RecordId, ip):
+def update_domain_name_value(record_id, ip):
     client = AcsClient(settings["accessKeyId"], settings["accessSecret"],
                        settings["regionId"])
     request = UpdateDomainRecordRequest()
     request.set_accept_format('json')
-    request.set_RecordId(RecordId)
+    request.set_RecordId(record_id)
     request.set_RR(settings["secondary_domain"])
     request.set_Type("A")
     request.set_Value(ip)
 
     response = client.do_action_with_exception(request)
-    print(str(response, encoding='utf-8'))
-    print("************更新完成************")
+    logging.info(str(response, encoding='utf-8'))
+    logging.info("更新完成")
 
 
 def get_domain_name_list():
@@ -79,16 +80,17 @@ def run():
     read_json_settings()
     while True:
         try:
-            update_ip = get_ip_addr()
+            new_ip = get_ip_addr()
             inquire = get_domain_name_list()
             current_ip = inquire[1]
-            RecordId = inquire[0]
-            if RecordId == "":
-                add_domain_name_value(ip=update_ip)
-            elif current_ip != update_ip:
-                update_domain_name_value(RecordId=RecordId, ip=update_ip)
+            record_id = inquire[0]
+            logging.info("new_ip: %s current_ip: %s record_id: %s", new_ip, current_ip, record_id)
+            if record_id == "":
+                add_domain_name_value(ip=new_ip)
+            elif current_ip != new_ip:
+                update_domain_name_value(record_id=record_id, ip=new_ip)
         except Exception as e:
-            print(e)
+            logging.exception(u"{}".format(e))
         time.sleep(settings["sleep_time"])
 
 
